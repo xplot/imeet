cdffc6cad9d745ec8ac8c11c2e8eeb63986b65b4
Javier de Paula <javi830810@gmail.com>
2014-10-23 13:38:14 -0400

    Removing unnecessary js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,30 +1,6 @@
 
 
-$(function() {
 
-    // First, checks if it isn't implemented yet.
-    if (!String.prototype.format) {
-      String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-          return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-          ;
-        });
-      };
-    }
-
-    jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
-        phone_number = phone_number.replace(/\s+/g, "");
-        return this.optional(element) || phone_number.length > 9 &&
-            phone_number.match(/^(\+?1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
-    }, "Please specify a valid phone number");
-
-
-    //Initialize Backbone see app.js
-    init_app();
-});
 
 
 
