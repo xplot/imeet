202dea3a0b1ca2220676109906f4bab9500f2e5c
ajadex <ajadex@gmail.com>
2015-01-10 17:24:27 -0500

    restyling of the page added the hangout section

--- static/js/views/invite.js
+++ static/js/views/invite.js
@@ -27,23 +27,21 @@ InviteView = Backbone.View.extend({
                 inviteDate.html(data.start);
 
                 var contact_html = "\
-            <div id='contact{3}' class='row contact-row small-margin' data-contact='{0},{1},{2}' > \
+            <div class='row contact-row small-margin {3}' data-contact='{0},{1},{2}' > \
                     <div class='col-sm-2'> {0} </div> \
                     <div class='col-sm-2'>  {1} </div> \
                     <div class='col-sm-2'> {2}</div> \
-                    <div class='col-sm-2'> {3}</div> \
-                    <div class='col-sm-2'> {4}</div> \
-                    <div class='col-sm-2'> {5}</div> \
             </div> ";
 
                 data.contacts.forEach(function(contact){
+                    var status = "";
+                    if(contact.sms_response != null || contact.voice_reponse != null || contact.email_response != null)
+                        status = "alert-success";
                     inviteTable.append(contact_html.format(
                         contact.name || 'N/A',
-                        contact.phone || 'N/A',
-                        contact.email || 'N/A',
-                        contact.sms_response || '<span class="text-danger">N/A</span>',
-                        contact.voice_response || '<span class="text-danger">N/A</span>',
-                        contact.email_response || '<span class="text-danger">N/A</span>'
+                        contact.email || '',
+                        contact.phone || '',
+                        status
                     ));
                 })
 
