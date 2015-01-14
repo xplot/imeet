b223c90bf4ac176a5ec898aae2a7a1f617e7f376
javi830810 <javier.depaula@kaplan.edu>
2015-01-14 10:36:17 -0500

    Changes to Invite Report

--- static/js/views/invite.js
+++ static/js/views/invite.js
@@ -6,6 +6,23 @@ InviteView = Backbone.View.extend({
         this.options = options || {};
         this.inviteId = this.options.id;
     },
+    contactToString: function(contact){
+        var contactString = "<div>{0} {1} {2} <div style='width: 50px;float: left' class='{3}'>YES</div></div>";
+
+        var confirmed = contact.sms_response != null
+            || contact.voice_response != null || contact.email_response!= null
+            || false;
+        var item_class = "hidden";
+        if(confirmed)
+            item_class = 'confirmed';
+
+        return contactString.format(
+                contact.name || '',
+                contact.phone || '',
+                contact.email || '',
+                item_class
+        );
+    },
     render: function(data){
         this.inviteId = data.invite_id;
 
@@ -13,7 +30,7 @@ InviteView = Backbone.View.extend({
             console.error('Invite Id is null, check routing');
 
         this.$el.html(this.template());
-
+        var that = this;
         $.ajax({
             url: "/api/invite/" + this.inviteId,
             type: "GET",
@@ -21,19 +38,20 @@ InviteView = Backbone.View.extend({
             success: function(data) {
                 var inviteTitle = $('.invite-title');
                 var inviteDate = $('.invite-date');
-                var inviteTable = $('.invite-table');
+                var inviteTable = $('.invite_report_attendees');
 
                 inviteTitle.html(data.title);
                 inviteDate.html(data.start);
 
                 var contact_html = "\
             <div id='contact{3}' class='row contact-row small-margin' data-contact='{0},{1},{2}' > \
-                    <div class='col-sm-2'> {0} </div> \
-                    <div class='col-sm-2'>  {1} </div> \
-                    <div class='col-sm-2'> {2}</div> \
-                    <div class='col-sm-2'> {3}</div> \
-                    <div class='col-sm-2'> {4}</div> \
-                    <div class='col-sm-2'> {5}</div> \
+                    <div class='col-sm-2  desktop'> {0} </div> \
+                    <div class='col-sm-2  desktop'>  {1} </div> \
+                    <div class='col-sm-2  desktop'> {2}</div> \
+                    <div class='col-sm-2  desktop'> {3}</div> \
+                    <div class='col-sm-2  desktop'> {4}</div> \
+                    <div class='col-sm-2  desktop'> {5}</div> \
+                    <div class='col-xs-12  mobile tablet'> {6}</div> \
             </div> ";
 
                 data.contacts.forEach(function(contact){
@@ -43,9 +61,21 @@ InviteView = Backbone.View.extend({
                         contact.email || 'N/A',
                         contact.sms_response || '<span class="text-danger">N/A</span>',
                         contact.voice_response || '<span class="text-danger">N/A</span>',
-                        contact.email_response || '<span class="text-danger">N/A</span>'
+                        contact.email_response || '<span class="text-danger">N/A</span>',
+                        that.contactToString(contact)
                     ));
-                })
+
+//                    for(var i=0;i<100;i++)
+//                        inviteTable.append(contact_html.format(
+//                        contact.name || 'N/A',
+//                        contact.phone || 'N/A',
+//                        contact.email || 'N/A',
+//                        contact.sms_response || '<span class="text-danger">N/A</span>',
+//                        contact.voice_response || '<span class="text-danger">N/A</span>',
+//                        contact.email_response || '<span class="text-danger">N/A</span>',
+//                        that.contactToString(contact)
+//                    ));
+                });
 
             }
         });
