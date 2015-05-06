b6baee682a7fd20b82e8cee31413d3cb8db0c796
ajadex <ajadex@gmail.com>
2015-05-06 00:37:39 -0400

    deploy script for settings

--- config/__init__.py
+++ config/__init__.py
@@ -6,17 +6,9 @@ It takes precedence over the config located in the boilerplate package.
 import os
 import logging
 
-if 'HTTP_HOST' in os.environ and os.environ['HTTP_HOST'] == "appengine.beecoss.com":
-    # Load Boilerplate config only in http://appengine.beecoss.com
-    # this code is here just for testing purposes
-    from boilerplate import config
-elif "SERVER_SOFTWARE" in os.environ:
-    if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
-        from localhost import config
-
-    elif os.environ['SERVER_SOFTWARE'].startswith('Google'):
-        from production import config
-    else:
-        raise ValueError("Environment undetected")
-else:
+if os.environ.get('imeet.testing') and "true" in os.environ['imeet.testing'].lower():
     from testing import config
+elif os.path.isfile("config/settings.py"):
+    from settings import config
+else:
+    raise ValueError("Environment undetected")
