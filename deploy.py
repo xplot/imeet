db00e8d8c39866d5096612190662893290bf9c83
ajadex <ajadex@gmail.com>
2015-05-06 00:19:08 -0400

    deploy script for settings

--- deploy.py
+++ deploy.py
@@ -1,4 +1,28 @@
 import os
+import fileinput
+import shutil
 
-print "this is a test1111"    
-print os.environ["GAE_OAUTH"]
+
+def environment_dict():
+    imeet_dict = {}
+    for variable in os.environ.keys():
+        variablel = variable.lower()
+        if "imeet." in variablel:
+            imeet_dict[variablel.replace("imeet.", "")] = os.environ[variablel]
+    return imeet_dict
+
+
+def get_existing_env_variable_in_line(line, imeet_dict):
+    for key in imeet_dict.keys():
+        if key in line:
+            return key
+    return None
+
+shutil.copy("config/settings.py.example", "config/settings.py")
+imeet_dict = environment_dict()
+for line in fileinput.input("config/settings.py", inplace=True):
+    env_variable = get_existing_env_variable_in_line(line, imeet_dict)
+    if env_variable:
+        print(line.replace(line[line.index(":"):-1], ":'" + imeet_dict[env_variable] + "',"))
+    else:
+        print(line)
