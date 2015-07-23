6d8a93bc378c01b4e64119036ae2bb54d8922f01
Ajadex Lopez <ajadex@gmail.com>
2015-07-23 10:13:37 -0400

    replacing / by - and converting the branch name to lowercase so it can be used as a version name in google app engine

--- tools/travis/deploy_to_google_app_engine.py
+++ tools/travis/deploy_to_google_app_engine.py
@@ -5,7 +5,9 @@ import os
 
 travis_home = sys.argv[1]
 travis_gae_oauth = sys.argv[2]
-travis_branch = sys.argv[3]
+travis_branch = sys.argv[3].replace("/", "-").lower()
+
+print travis_branch
 
 appCfg = "%s/appEngine/google_appengine/appcfg.py --oauth2_refresh_token=%s --version=%s update ."%(
     travis_home,
