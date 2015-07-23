134c0f369266e7f239ff24cd7a4c16cc09ad7b5c
Ajadex Lopez <ajadex@gmail.com>
2015-07-23 10:07:38 -0400

    introducing deploy_to_google_app_engine.py so we can customize the deployment to Google App Engine

--- tools/travis/deploy_to_google_app_engine.py
+++ tools/travis/deploy_to_google_app_engine.py
@@ -3,9 +3,9 @@ __author__ = 'ajadex'
 import sys
 import os
 
-travis_home = sys.argv(1)
-travis_gae_oauth = sys.argv(2)
-travis_branch = sys.argv(3)
+travis_home = sys.argv[1]
+travis_gae_oauth = sys.argv[2]
+travis_branch = sys.argv[3]
 
 appCfg = "%s/appEngine/google_appengine/appcfg.py --oauth2_refresh_token=%s --version=%s update ."%(
     travis_home,
@@ -13,3 +13,4 @@ appCfg = "%s/appEngine/google_appengine/appcfg.py --oauth2_refresh_token=%s --ve
     travis_branch
 )
 os.system(appCfg)
+
